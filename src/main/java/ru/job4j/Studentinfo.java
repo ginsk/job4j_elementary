6f2c5b4f1202fb81a99557abe28cab2e401f4307
ginsk <gin2003@gmail.com>
2021-12-31 12:02:43 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Gin Karg");
         System.out.println("08.09.1964");
     }
