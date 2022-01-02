9c23be66815d44a565a287d42dcdcc51670b4180
ginsk <gin2003@gmail.com>
2022-01-02 13:49:17 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,12 +2,12 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x1 - x2) + Math.pow(y1 - y2));
+        double rsl = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
         return rsl;
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
+       double result = Point.distance(0, 0, 2, 0);
         System.out.println("resul (x1, y1) to (x2,y2)" + result);
     }
 }
