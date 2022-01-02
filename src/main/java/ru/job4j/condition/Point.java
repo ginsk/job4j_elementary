5ae87296f841c975c2b4a874b99f09c1b43eae50
ginsk <gin2003@gmail.com>
2022-01-02 13:03:04 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,12 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl1 = (x1 - x2);
-        double rsl2 = (y1 - y2);
-        rsl1 = Math.pow(rsl1, 2);
-        rsl2 = Math.pow(rsl2, 2);
-        double rsl = rsl1 + rsl2;
-        rsl = Math.sqrt(rsl);
+        double rsl = Math.sqrt(Math.pow(x1 - x2) + Math.pow(y1 - y2));
         return rsl;
     }
 
---
+++ src/main/java/ru/job4j/condition/ScArea.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class ScArea {
+
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double s = l * h;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        double result1 = ScArea.square(6, 2);
+        System.out.println(result1);
+    }
+}
