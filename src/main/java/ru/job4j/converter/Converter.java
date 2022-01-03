cdb8dcd51bc977d9cd82cbdf3cc144be045a95fb
ginsk <gin2003@gmail.com>
2022-01-03 09:12:47 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -17,5 +17,15 @@ public class Converter {
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are" + euro + "euro.");
         System.out.println("140 rubles are" + dollar + "dollar.");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euro. Test result :" + passed);
+        in = 180;
+        expected = 3;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("180 rubles are 3 dollars. Test result :" + passed);
     }
 }
