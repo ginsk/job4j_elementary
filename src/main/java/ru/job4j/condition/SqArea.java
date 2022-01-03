15cc430196bdb9bb09a5bfa808fa55303e984896
ginsk <gin2003@gmail.com>
2022-01-03 19:04:12 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -1,6 +1,6 @@
 package ru.job4j.condition;
 
-public class ScArea {
+public class SqArea {
 
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
@@ -10,7 +10,7 @@ public class ScArea {
     }
 
     public static void main(String[] args) {
-        double result1 = ScArea.square(6, 2);
+        double result1 = SqArea.square(6, 2);
         System.out.println("p = 6, k = 2, real = " + result1);
     }
  }
