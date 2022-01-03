31cc3a302b9fbe9f8361a031a6ff4008cd2357b5
ginsk <gin2003@gmail.com>
2022-01-03 10:53:06 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140rblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert180rblThen3Dollar() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
