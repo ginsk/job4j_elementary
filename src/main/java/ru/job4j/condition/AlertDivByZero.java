c847bf17a226ee8fbc90567e1ceae48e0f5797ea
ginsk <gin2003@gmail.com>
2022-01-03 20:14:47 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+
+    public static void possibleDiv(int number) {
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
+        if (number < 0) {
+            System.out.println("This is negative numbers");
+        }
+    }
+}
