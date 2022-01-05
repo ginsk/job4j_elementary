271462734329f1d35380aff435f7dd634f68178b
ginsk <gin2003@gmail.com>
2022-01-05 08:54:15 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -16,10 +16,10 @@ public class TriangleTest {
 
     @Test
     public void whenNotExist() {
-        double ab = 2.0;
-        double ac = 2.0;
-        double bc = 2.0;
-        boolean result = !Triangle.exist(ab, ac, bc);
+        double ab = 3.0;
+        double ac = 1.0;
+        double bc = 1.0;
+        boolean result = Triangle.exist(ab, ac, bc);
         Assert.assertFalse(result);
     }
 }
