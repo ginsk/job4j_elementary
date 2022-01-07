d966280af1b2495a50b6a496f72d634efc7baf47
ginsk <gin2003@gmail.com>
2022-01-07 10:52:46 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,14 +3,17 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int number) {
-        for (int i = 1; i < number; i++) {
+        if (number == 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-             boolean prime = false;
-             return prime;
-             break;
+                return false;
             }
         }
-        boolean prime = true;
-        return prime;
-    }
-}
+        return true;
+        }
+
+ }
+
+
