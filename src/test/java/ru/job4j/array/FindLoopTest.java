5ff109fef7a1ee575bc1a2938ece9973d62b3269
ginsk <gin2003@gmail.com>
2022-01-08 16:20:26 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -15,11 +15,11 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHas6Then2() {
+    public void whenArrayDoesntHave5ThenReturnMinus() {
         int[] data = {1, 2, 3, 4};
-        int el = 4;
+        int el = 5;
         int result = FindLoop.indexOf(data, el);
-        int expected = 3;
+        int expected = -1;
         Assert.assertEquals(expected, result);
     }
 }
