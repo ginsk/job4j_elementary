7fa592eddee1fae9224c114b823178270af651c8
ginsk <gin2003@gmail.com>
2022-01-09 18:51:42 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class EndsWith {
+
+    public static boolean endsWith(char[] word, char[] post) {
+        for (int i = 0; i < word .length; i++) {
+            if (word[word.length - 1 - i] == post[post.length - 1 - i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
