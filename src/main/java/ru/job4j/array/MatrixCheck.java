43a10f30fad6f4337031439561fe4ab0bd88ac43
ginsk <gin2003@gmail.com>
2022-01-11 17:29:47 +0300

    3.2. Загрузка файлов на github [#243562 #244884] 3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -14,12 +14,12 @@ public class MatrixCheck {
 
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
-        for (int j = 0; j < board.length; j++) {
-            if (board[j][column] != 'X') {
-                result = false;
-                break;
+             for (int j = 0; j < board.length; j++) {
+                if (board[j][column] != 'X') {
+                    result = false;
+                    break;
+                }
             }
-        }
         return result;
     }
 
